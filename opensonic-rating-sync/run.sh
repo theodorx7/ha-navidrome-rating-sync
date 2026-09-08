827d49614a3e8f93cf78bd21f016223f44e9ae97
Fedor <99128059+theodorx7@users.noreply.github.com>
2026-09-08 21:01:43 +0700

    Update run.sh

--- opensonic-rating-sync/run.sh
+++ opensonic-rating-sync/run.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/with-contenv bashio
+
 set -e
-
 bashio::log.info "Starting Navidrome/Subsonic Rating Sync App..."
-
 export PYTHONPATH="/app/"
-
 exec python3 -u -m opensonic_rating_sync
